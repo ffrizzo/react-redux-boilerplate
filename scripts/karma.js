9cb1cdd8b26015c96508f36d092128e1bea30a35
Felipe Frizzo <felipefrizzo@gmail.com>
2018-01-04 14:37:31 -0200

    Fixed lint error's of scripts and configs

--- scripts/karma.js
+++ scripts/karma.js
@@ -1,3 +1,3 @@
-require ('babel-register')
+require ('babel-register');
 
-module.exports = require('../configs/karma/karma.conf')
+module.exports = require('../configs/karma/karma.conf');
--- scripts/server.js
+++ scripts/server.js
@@ -1,10 +1,10 @@
-require('babel-register')
+require('babel-register');
 
-const server = require('../server/main')
+const server = require('../server/main');
 
-const host = process.env.HOST || 'localhost'
-const port = process.env.PORT || 3000
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3000;
 
 server.listen(port, host, function(){
-    console.log('Server listening on ==> ' + host + ":" + port)
-})
+  console.log('Server listening on ==> ' + host + ":" + port);
+});
