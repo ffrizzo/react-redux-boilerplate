2c437af9fc975a00c47a6efd72b38af69b6cdffa
Felipe Frizzo <felipefrizzo@gmail.com>
2018-01-04 14:39:34 -0200

    Fixed lint error's of containers, store and reducers

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,37 +1,40 @@
-import thunk from 'redux-thunk'
-import rootReducer from '../reducers'
-import { applyMiddleware, compose, createStore } from 'redux'
-import { routerMiddleware } from 'react-router-redux'
+import thunk from 'redux-thunk';
+import { routerMiddleware } from 'react-router-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+
+import rootReducer from '../reducers';
 
 export default function configureStore(history, initialState) {
-    let devTools = []
-    if (__DEBUG__ ) {
-      if (window.devToolsExtension){
-        //If devToolsExtension is present
-        //configure to use extension
-        devTools = [ window.devToolsExtension() ]
-      }else{
-        devTools = [ require('containers/dev-tools').instrument() ]
-      }
+  let devTools = [];
+  if (__DEBUG__) {
+    if (window.devToolsExtension) {
+      // If devToolsExtension is instaled on chrome configure to use the extension
+      devTools = [window.devToolsExtension()];
+    } else {
+      /* eslint-disable global-require */
+      devTools = [require('../containers/dev-tools').instrument()];
+      /* eslint-enable global-require */
     }
+  }
 
-    let middlewares = [ thunk, routerMiddleware(history) ]
+  const middlewares = [thunk, routerMiddleware(history)];
 
-    const store = createStore(
-        rootReducer,
-        initialState,
-        compose(
-          applyMiddleware(...middlewares),
-          ...devTools
-        )
-    )
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middlewares),
+      ...devTools,
+    ),
+  );
 
-    if(module.hot){//Hot reload for development mode
-        module.hot.accept('../reducers', () => {
-            const nextRootReducer = require('../reducers')
-            store.replaceReducer(nextRootReducer)
-        })
-    }
-
-    return store
+  if (module.hot) { // Hot reload to improve development mode
+    module.hot.accept('../reducers', () => {
+      /* eslint-disable global-require */
+      const nextRootReducer = require('../reducers');
+      /* eslint-enable global-require */
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+  return store;
 }
