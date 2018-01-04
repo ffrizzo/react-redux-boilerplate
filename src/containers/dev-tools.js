f460c9f081bb7370db3eee281e1055df9079c09a
Felipe Frizzo <felipefrizzo@gmail.com>
2018-01-04 14:40:04 -0200

    Fixed lint error's of src components

--- src/containers/dev-tools.js
+++ src/containers/dev-tools.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import { createDevTools } from 'redux-devtools'
-import LogMonitor from 'redux-devtools-log-monitor'
-import DockMonitor from 'redux-devtools-dock-monitor'
+import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import { createDevTools } from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
+/* eslint-enable import/no-extraneous-dependencies */
 
+/* eslint function-paren-newline: ["error", "always"] */
 export default createDevTools(
-    <DockMonitor
-        toggleVisibilityKey='ctrl-h'
-        changePositionKey='ctrl-q'>
-        <LogMonitor />
-    </DockMonitor>
-)
+  <DockMonitor
+    toggleVisibilityKey="ctrl-h"
+    changePositionKey="ctrl-q"
+  >
+    <LogMonitor />
+  </DockMonitor>,
+);
